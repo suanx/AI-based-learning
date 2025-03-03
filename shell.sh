0ab403e9814eb74405b8f6377691d3818f261260
SuanX <sujiinfinity96@gmail.com>
2025-03-03 22:36:35 +0530

    shell script basic file created

---
+++ shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello, Sujan! This is your first Jenkins pipeline shell script."
+echo "Current Date and Time: $(date)"
+echo "System Uptime: $(uptime -p)"
+echo "Listing Files in Workspace:"
+ls -l
+
